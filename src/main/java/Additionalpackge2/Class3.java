17d7b540dfb034c14b2bf3182bdede672102d8a2
Andrii <andrii.pikhur@gmail.com>
2022-08-01 12:46:25 +0300

    v3.0 Additional Class Added Additional function added to main method "Third  upgrade of app"

---
+++ src/main/java/Additionalpackge2/Class3.java
@@ -0,0 +1,4 @@
+package Additionalpackge2;
+
+public class Class3 {
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Second upgrade of app");
+
+        System.out.println("Third  upgrade of app");
     }
 }
